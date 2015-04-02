44757a2a2053b7e40acc5a717ca9e0bd1c775579
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-04-02 13:12:42 +0300

    Added create command for creating repos via httpie + GH auth token

--- gh.sh
+++ gh.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# This file will have a line like
+# 'export GITHUB_REPO_TOKEN="token_value"'
+if [[ -r "$GITHUB_REPO_TOKEN_LOC" ]]; then
+	source "$GITHUB_REPO_TOKEN_LOC"
+else
+	exit 1
+fi
+
+HTTPIE_FLAGS="--print b"
+
 COMMAND=$1
-USER=$2
 
 case "$COMMAND" in
 	cl*)
-		git clone git@github.com:$USER/$3.git
+		USER="$2"
+		REPONAME="$3"
+		git clone "git@github.com:$USER/$3.git"
 		;;
+	cr*)
+		REPONAME="$2"
+		http $HTTPIE_FLAGS "https://api.github.com/user/repos" "Authorization:token $GITHUB_REPO_TOKEN" "name=$REPONAME"
 esac
